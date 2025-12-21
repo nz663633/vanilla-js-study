76905f3ddeb6578ceae947d7e2345517cf39ea42
nz663633 <hyunjisim3@gmail.com>
2025-12-21 23:34:59 +0900

    feat: add API request with async/await

---
+++ src/api.js
@@ -0,0 +1,7 @@
+const getData = async () => {
+    let response = await fetch('https://jsonplaceholder.typicode.com/users')
+    let data = await response.json();
+    console.log(data);
+}
+
+getData();
