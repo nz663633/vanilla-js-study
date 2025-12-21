0a01bcd83b34ee95e8c16226d8ea0c4942a977cd
nz663633 <hyunjisim3@gmail.com>
2025-12-21 23:38:26 +0900

    feat: add try/catch to async/await API call

--- src/api.js
+++ src/api.js
@@ -1,7 +1,11 @@
 const getData = async () => {
-    let response = await fetch('https://jsonplaceholder.typicode.com/users')
-    let data = await response.json();
-    console.log(data);
-}
+    try {
+        let response = await fetch('https://jsonplaceholder.typicode.com/users')
+        let data = await response.json();
+        console.log(data);
+    } catch (err) {
+        console.log(err);
+    }
+};
 
 getData();
