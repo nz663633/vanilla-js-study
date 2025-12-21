6275369aff654a8d07dd19d20d806adcd4f7b04d
nz663633 <hyunjisim3@gmail.com>
2025-12-21 21:04:03 +0900

    docs: study Promise then and catch

--- src/promise.js
+++ src/promise.js
@@ -6,11 +6,12 @@ const executor = (resolve, reject) => {
 };
 
 const promise = new Promise(executor);
-promise.then(
-    (result) => {
-        console.log(result)
-    },
-    (error) => {
-        console.log(error)
-    }
-); 
+promise
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+// then 메서드: promise가 fulfilled(성공) 되었을 때 실행
+// catch 메서드: promise가 reject(실패) 되었을 때 실행
