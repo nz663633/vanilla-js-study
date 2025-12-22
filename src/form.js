fabcd6caef77d206493578b103d08b3be04873df
nz663633 <hyunjisim3@gmail.com>
2025-12-22 14:27:07 +0900

    feat: log password on input

--- src/form.js
+++ src/form.js
@@ -1,9 +1,8 @@
 const $userName = document.getElementById('userName');
 const $password = document.getElementById('password');
 
-const $loginBtn = document.querySelector('button');
+$userName.value = '아무개';
 
-$loginBtn.addEventListener('click', () => {
-    console.log($userName.value);
-    console.log($password.value);
+$password.addEventListener('input', (event) =>{
+    console.log(event.target.value);
 })
