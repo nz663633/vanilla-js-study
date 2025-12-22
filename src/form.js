b6363f3fa61878d924ed0899a1c39cdc1501364b
nz663633 <hyunjisim3@gmail.com>
2025-12-22 14:22:12 +0900

    feat: log userName and password on loginBtn click

--- src/form.js
+++ src/form.js
@@ -1,8 +1,9 @@
-const $fruitSelect = document.getElementById('fruitSelect');
+const $userName = document.getElementById('userName');
+const $password = document.getElementById('password');
 
-$fruitSelect.addEventListener('change', (event) => {
-    console.log(event.target.value);
-    // 값을 변경할 때마다 콘솔에 해당 value 값 출력
-});
+const $loginBtn = document.querySelector('button');
 
-$fruitSelect.remove(1); // 1이라는 인덱스를 가진 바나나 삭제
+$loginBtn.addEventListener('click', () => {
+    console.log($userName.value);
+    console.log($password.value);
+})
