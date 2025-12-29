b07eb6608fe671240e94cffbe1d6cc7c2908dde9
nz663633 <hyunjisim3@gmail.com>
2025-12-29 12:25:53 +0900

    feat: structure app state management

--- animal-album/src/app.js
+++ animal-album/src/app.js
@@ -2,8 +2,46 @@
 
 import TabBar from './components/TabBar.js';
 import Content from './components/Content.js';
+import { request } from './components/api.js';
 
 export default function App($app) {
-    const tabBar = new TabBar();
+    this.state = { // 초기 상태 값
+        currentTab: 'all', // 현재 선택된 탭의 이름
+        photos: [], // 현재 화면에 보여줄 사진 데이터
+    }; // -> app이 관리하는 전역 상태
+
+    const tabBar = new TabBar({
+        $app,
+        initialState: '', // TabBar 내부에서 사용할 초기 상태(선택된 탭)
+        onClick: async (name) => { // name에는 클릭한 탭이름(penguin, koala, panda)
+            this.setState({
+                ...this.state,
+                currentTab: name, // 클릭한 탭으로 변경
+                photos: await request(name) // 클릭한 탭에 맞는 사진 데이터 요청
+            });
+        } // 탭 클릭 -> 상태 변경
+    });
+
     const content = new Content();
+
+    this.setState() = (newState) => {
+        this.state = newState;
+        tabBar.setState(this.state.currentTab); // 변경된 currentTab을 TabBar에 전달
+        content.setState(this.state.photos); // 변경된 photos를 Content에 전달
+    };
+
+    const init = async () => { // 처음 실행될 때 한 번만 실행되는 초기화 함수
+        try{
+            const initialPhotos = await request(); // 아무 탭도 안눌렀을 때 기본 사진 요청
+            this.setState({
+                ...this.state,
+                photos: initialPhotos
+            });
+            // 초기 사진 데이터를 저장
+        } catch(err) {
+            console.log(err);
+        }
+    };
+
+    init(); // app 생성되자마자 초기 데이터 로딩 실행
 }
--- animal-album/src/components/api.js
+++ animal-album/src/components/api.js
@@ -1,12 +1,12 @@
 const API_URL = 'https://animal-api-two.vercel.app/';
 
 // API
-export const request = async () => {
+export const request = async () => { // 서버에서 동물 사진 데이터 받아오기
     let res = await fetch(API_URL); // 요청 시작, res는 Promise 객체
     try {
-        if (res) {
+        if (res) { // 응답이 있으면 진행
             let data = await res.json(); // res에서 JSON 데이터를 파싱하는 작업이 완전히 끝날 때까지 기다림
-            return data.photos;
+            return data.photos; // data에서 photos에 해당하는 데이터 반환
         }
     } catch (err) {
         console.log(err);
