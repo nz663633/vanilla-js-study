225967a6476e4c7c7453829536cce3f9323ca660
nz663633 <hyunjisim3@gmail.com>
2025-12-29 16:28:53 +0900

    feat: add History API navigation to animal album

--- animal-album/src/app.js
+++ animal-album/src/app.js
@@ -6,44 +6,54 @@ import { request } from './components/api.js';
 
 export default function App($app) {
     this.state = { // 초기 상태 값
-        currentTab: 'all', // 현재 선택된 탭의 이름
+        currentTab: window.location.pathname.replace('/', '') || "all", // 현재 선택된 탭의 이름
         photos: [], // 현재 화면에 보여줄 사진 데이터
     }; // -> app이 관리하는 전역 상태
 
+    // tab
     const tabBar = new TabBar({
         $app,
         initialState: '', // TabBar 내부에서 사용할 초기 상태(선택된 탭)
         onClick: async (name) => { // name에는 클릭한 탭이름(penguin, koala, panda)
-            this.setState({
-                ...this.state,
-                currentTab: name, // 클릭한 탭으로 변경
-                photos: await request(name === "all"?'':name) // 클릭한 탭에 맞는 사진 데이터 요청
-            });
-        } // 탭 클릭 -> 상태 변경
+            history.pushState(null, `${name} 사진`, name);
+            this.updateContent(name);
+        }
     });
 
+    // content
     const content = new Content({
         $app,
         initialState: [],
     });
 
+    // state
     this.setState = (newState) => {
         this.state = newState;
         tabBar.setState(this.state.currentTab); // 변경된 currentTab을 TabBar에 전달
         content.setState(this.state.photos); // 변경된 photos를 Content에 전달
     };
 
-    const init = async () => { // 처음 실행될 때 한 번만 실행되는 초기화 함수
+    this.updateContent = async (tabName) => {
         try {
-            const initialPhotos = await request(); // 아무 탭도 안눌렀을 때 기본 사진 요청
+            const currentTab = tabName === "all" ? '' : tabName;
+            const photos = await request(currentTab); // 아무 탭도 안눌렀을 때 기본 사진 요청
             this.setState({
                 ...this.state,
-                photos: initialPhotos
+                currentTab: tabName,
+                photos: photos
             });
             // 초기 사진 데이터를 저장
         } catch (err) {
             console.log(err);
         }
+    }
+
+    window.addEventListener('popstate', async () => {
+        this.updateContent(window.location.pathname.replace('/', ''));
+    });
+
+    const init = async () => { // 처음 실행될 때 한 번만 실행되는 초기화 함수
+        this.updateContent(this.state.currentTab);
     };
 
     init(); // app 생성되자마자 초기 데이터 로딩 실행
