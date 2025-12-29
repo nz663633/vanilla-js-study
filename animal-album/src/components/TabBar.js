4a5d6cf91cbc17c652bb4ff803d946be341f7333
nz663633 <hyunjisim3@gmail.com>
2025-12-29 14:32:04 +0900

    feat: update TabBar component

--- animal-album/src/components/TabBar.js
+++ animal-album/src/components/TabBar.js
@@ -2,9 +2,9 @@ export default function TabBar({$app, initialState, onClick}) {
     this.state = initialState;
     this.onClick = onClick;
 
-    this.$target = document.createElement('div');
+    this.$target = document.createElement('div'); // TabBar 전체를 감싸는 DOM 생성
     this.$target.className = 'tab-bar';
-    $app.appendChild(this.$target);
+    $app.appendChild(this.$target); // TabBar는 app의 자식 컴포넌트
 
     this.template = () => {
         let temp = `<div id="all">전체</div><div id="penguin">펭귄</div>
@@ -19,11 +19,12 @@ export default function TabBar({$app, initialState, onClick}) {
         let $currentTab = document.getElementById(this.state);
         // $currentTab ? ($currentTab.className = 'clicked'): '';
         $currentTab && ($currentTab.className = 'clicked');
+        // ->  해당 탭이 존재하면 clicked 클래스 추가
 
         const $tabBar = this.$target.querySelectorAll('div');
         $tabBar.forEach((elm) => {
             elm.addEventListener('click', () => {
-                onClick(elm.id);
+                onClick(elm.id); // 해당 id(탭)가 눌렸다는 것을 app에 전달
             })
         })
     };
