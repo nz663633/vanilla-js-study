54a9439731e2b10e73404b8a8b10f22c05bce294
nz663633 <hyunjisim3@gmail.com>
2026-01-03 20:09:53 +0900

    refactor: update Header component logic

--- trip-wiki/src/App.js
+++ trip-wiki/src/App.js
@@ -8,10 +8,23 @@ import CityDetail from "./components/CityDetail.js";
 import { request } from "./components/api.js";
 
 export default function App($app) {
+    const getSortBy = () => {
+        if (window.location.search){
+            return window.location.search.split('sort=')[1].split('&')[0];
+        }
+        return 'total';
+    };
+    const getSearchWord = () => {
+        if (window.location.search && window.location.search.includes('search=')){
+            return window.location.search.split('search=')[1];
+        }
+        return '';
+    }
+
     this.state = {
         startIdx: 0,
-        sortBy: '',
-        searchWord: '',
+        sortBy: getSortBy(),
+        searchWord: getSearchWord(),
         region: '',
         cities: '',
     };
@@ -32,6 +45,16 @@ export default function App($app) {
                 startIdx: 0,
                 sortBy: sortBy,
                 cities: cities
+            });
+        },
+        handleSearch: async (searchWord) => {
+            history.pushState(null, null, `/${this.state.region}?sort=${this.state.sortBy}&search=${searchWord}`);
+            const cities = await request(0, this.state.region, this.state.sortBy, searchWord);
+            this.setState({
+                ...this.state,
+                startIdx: 0,
+                searchWord: searchWord,
+                cities: cities
             })
         }
     });
@@ -45,7 +68,7 @@ export default function App($app) {
                 ...this.state,
                 startIdx: newStartIdx,
                 cities: {
-                    cities: [...this.state.cities, ...newCities.cities],
+                    cities: [...this.state.cities.cities, ...newCities.cities],
                     isEnd: newCities.isEnd
                 }
             })
@@ -56,6 +79,7 @@ export default function App($app) {
     this.setState = (newState) => {
         this.state = newState;
         cityList.setState(this.state.cities);
+        header.setState({ sortBy: this.state.sortBy, searchWord: this.state.searchWord });
     }
 
     const init = async () => {
